55dcad7f0a18eb66978f9ddb64c2fd0287d37ffa
Aymeric Anger <daedelus.git@gmail.com>
2026-05-24 18:33:57 +0200

    :bookmark: 2026.05 :sparkles: speak command  on Windows os

--- config/command.php
+++ config/command.php
@@ -3,16 +3,17 @@
 use Command\ClearCacheCommand;
 use Command\ClearTranslationCacheCommand;
 use Command\HelloCommand;
+use Command\SpeakCommand;
 use Command\TranslationGeneratorCommand;
 use NGSOFT\Console\ConsoleApplication;
 
 /**
  * Register your commands there.
  */
-return function (ConsoleApplication $app)
-{
+return function (ConsoleApplication $app) {
     $app->add(HelloCommand::class);
     $app->add(TranslationGeneratorCommand::class);
     $app->add(ClearTranslationCacheCommand::class);
     $app->add(ClearCacheCommand::class);
+    $app->add(SpeakCommand::class);
 };
---
+++ src/Command/SpeakCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Command;
+
+
+use NGSOFT\Console\Profile\CommandHelper;
+use Provider\SynthesisProviderStack;
+use SpeechSynthesis\SpeechSynthesisUtterance;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Process\Process;
+use Traits\CommandTrait;
+
+#[AsCommand('speak', 'Generate audio and speak')]
+class SpeakCommand extends Command
+{
+    use CommandTrait;
+
+
+    public function __construct(private readonly SynthesisProviderStack $synthesisProviderStack)
+    {
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->addArgument('text', mode: InputArgument::REQUIRED);
+    }
+
+    protected function executeCommand(CommandHelper $io, InputInterface $input)
+    {
+
+
+        $text = $input->getArgument('text');
+
+        if (!$text) {
+            $io->error("Text argument is empty");
+        }
+
+
+        $voice = "en-US-AvaMultilingualNeural";
+        $lang = "en-US";
+        $format = 'mp3';
+
+        if ('\\' === DIRECTORY_SEPARATOR) {
+            $result = $this->synthesisProviderStack->speak(
+                SpeechSynthesisUtterance::make(compact('voice', 'text', 'lang', 'format')));
+
+            if ($result->path) {
+                try {
+                    $proc = Process::fromShellCommandline(
+                        sprintf('"%s/cmdmp3.exe" "%s"', resolve_path('%project_root%/bin'), $result->path)
+                    );
+                    $proc->run();
+                    return $proc->getExitCode();
+
+                } finally {
+                    @unlink($result->path);
+                }
+
+            }
+        }
+        return self::FAILURE;
+    }
+}
